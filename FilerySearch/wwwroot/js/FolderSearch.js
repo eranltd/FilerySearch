e9442e2bde453866b1ccd3cbfb7402268e92558c
Eran peled <eranltd@gmail.com>
2019-03-23 16:44:37 +0200

    dateTimePicker has replaced and working. due to asp-append-version="true"

---
+++ FilerySearch/wwwroot/js/FolderSearch.js
@@ -0,0 +1,25 @@
+﻿
+//(function () {
+    
+//})();
+
+
+//if ($.fn.datetimepicker) {
+//    alert("$('.datetimepicker').datetimepicker();");
+//}
+
+$(document).ready(function () {
+    $("#frmDate").datetimepicker();
+    //$("#toDate").datetimepicker();
+});
+
+//document.getElementById("filepicker").addEventListener("change", function (event) {
+//    let output = document.getElementById("queryResult");
+//    let files = event.target.files;
+
+//    for (let i = 0; i < files.length; i++) {
+//        let item = document.createElement("li");
+//        item.innerHTML = files[i].webkitRelativePath;
+//        output.appendChild(item);
+//    };
+//}, false);
