1cf2d9a9fc57510abbe0878b13ee59f6b6531803
Eran peled <eranltd@gmail.com>
2019-03-23 17:52:25 +0200

    stable

--- FilerySearch/wwwroot/js/FolderSearch.js
+++ FilerySearch/wwwroot/js/FolderSearch.js
@@ -1,7 +1,29 @@
-﻿
-//(function () {
-    
-//})();
+﻿var filesList = undefined;
+
+(function () {
+    document.getElementById("filepicker").addEventListener("change", function (event) {
+        //let output = document.getElementById("queryResult");
+        let files = event.target.files;
+        filesList = files;
+        //for (let i = 0; i < files.length; i++) {
+        //    //let item = document.createElement("li");
+        //    //item.innerHTML = files[i].webkitRelativePath;
+        //    //output.appendChild(item);
+        //    filesList.push();
+        //};
+    }, false);
+})();
+
+function SearchOccurences() {
+    let output = document.getElementById("queryResult");
+    for (let i = 0; i < filesList.length; i++) {
+        let item = document.createElement("li");
+        item.innerHTML = filesList[i].webkitRelativePath;
+        output.appendChild(item);
+    };
+
+}
+
 
 
 //if ($.fn.datetimepicker) {
@@ -10,16 +32,9 @@
 
 $(document).ready(function () {
     $("#frmDate").datetimepicker();
-    //$("#toDate").datetimepicker();
+    $("#toDate").datetimepicker();
+
+
 });
 
-//document.getElementById("filepicker").addEventListener("change", function (event) {
-//    let output = document.getElementById("queryResult");
-//    let files = event.target.files;
 
-//    for (let i = 0; i < files.length; i++) {
-//        let item = document.createElement("li");
-//        item.innerHTML = files[i].webkitRelativePath;
-//        output.appendChild(item);
-//    };
-//}, false);
